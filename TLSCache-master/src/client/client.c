7e8a7ba0ef7fa05dccdb6b08744aa538bc544249
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2020-06-10 10:59:00 -0700

    Update client.c

--- TLSCache-master/src/client/client.c
+++ TLSCache-master/src/client/client.c
@@ -103,9 +103,9 @@ int main(int argc, char *argv[])
 	//added ASSIGN FOR FILENAME
 	unsigned char *filename = argv[3];
 
-	printf("WIEGHT: %d\n",weight(filename,9998));
-	printf("FILENAME: %s\n", filename);
-	printf("PROXY NUM: %d\n", proxyNum(filename));
+// 	printf("WIEGHT: %d\n",weight(filename,9998));
+// 	printf("FILENAME: %s\n", filename);
+// 	printf("PROXY NUM: %d\n", proxyNum(filename));
 	/* now safe to do this */
 	port = p;
 	//printf(filename);
