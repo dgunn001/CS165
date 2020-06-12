e7d9d577a657848b938e01fae6d65b5d5248c010
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2020-06-12 10:37:41 -0700

    Update proxy.c

--- TLSCache-master/src/proxy/proxy.c
+++ TLSCache-master/src/proxy/proxy.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <math.h>
+#include <pthread.h>
 
 #include <tls.h>
 
@@ -115,135 +116,8 @@ static void kidhandler(int signum) {
 	waitpid(WAIT_ANY, NULL, WNOHANG);
 }
 
-
-int main(int argc,  char *argv[])
-{
-	struct sockaddr_in sockname, client, server_sa;
-	char buffer[80], *ep;
-	size_t maxread;
-	struct sigaction sa;
-	int serverCall[1] = {0};
-	int sd, i, ssd;
-	char bloom[40] = {0};
-	unsigned int fileLen;
-	socklen_t clientlen;
-	u_short port;
-	u_short serverport;
-	pid_t pid;
-	u_long p;
-	u_long sp;
-	struct tls_config *tls_cfg = NULL; // TLS config
-	struct tls_config *tls_scfg = NULL; //TLS server config
-	struct tls *tls_ctx = NULL; // TLS context
-	struct tls *tls_cctx = NULL; // client's TLS context
-	struct tls *tls_sctx = NULL; // server's TLS context
-	
-	
-	/*
-	 * first, figure out what port we will listen on - it should
-	 * be our first parameter.
-	 */
-
-	if (argc != 3)
-		usage();
-		errno = 0;
-        p = strtoul(argv[1], &ep, 10);
-	
-        if (*argv[1] == '\0' || *ep != '\0') {
-		/* parameter wasn't a number, or was empty */
-		fprintf(stderr, "%s - not a number\n", argv[1]);
-		usage();
-	}
-        if ((errno == ERANGE && p == ULONG_MAX) || (p > USHRT_MAX)) {
-		/* It's a number, but it either can't fit in an unsigned
-		 * long, or is too big for an unsigned short
-		 */
-		fprintf(stderr, "%s - value out of range\n", argv[1]);
-		usage();
-	}
-	
-	sp = strtoul(argv[2], &ep, 10);      
-	if (*argv[2] == '\0' || *ep != '\0') {
-		/* parameter wasn't a number, or was empty */
-		fprintf(stderr, "%s - not a number\n", argv[2]);
-		usage();
-	}
-        if ((errno == ERANGE && sp == ULONG_MAX) || (sp > USHRT_MAX)) {
-		/* It's a number, but it either can't fit in an unsigned
-		 * long, or is too big for an unsigned short
-		 */
-		fprintf(stderr, "%s - value out of range\n", argv[2]);
-		usage();
-	}
-	/* now safe to do this */
-	port = p;
-	serverport = sp;
-
-	/* set up TLS */
-	if ((tls_cfg = tls_config_new()) == NULL)
-		errx(1, "unable to allocate TLS config");
-	if (tls_config_set_ca_file(tls_cfg, "/home/csmajs/dgunn001/CS165/TLSCache-master/certificates/root.pem") == -1)
-		errx(1, "unable to set root CA file");
-	if (tls_config_set_cert_file(tls_cfg, "/home/csmajs/dgunn001/CS165/TLSCache-master/certificates/server.crt") == -1) 
-		errx(1, "unable to set TLS certificate file, error: (%s)", tls_config_error(tls_cfg));
-	if (tls_config_set_key_file(tls_cfg, "/home/csmajs/dgunn001/CS165/TLSCache-master/certificates/server.key") == -1)
-		errx(1, "unable to set TLS key file");
-	if ((tls_ctx = tls_server()) == NULL)
-		errx(1, "TLS server creation failed");
-	if (tls_configure(tls_ctx, tls_cfg) == -1)
-		errx(1, "TLS configuration failed (%s)", tls_error(tls_ctx));
-
-	/* the message we send the client */
-	strncpy(buffer,
-	    "It was the best of times, it was the worst of times... \n",
-	    sizeof(buffer));
-
-	
-	memset(&sockname, 0, sizeof(sockname));
-	sockname.sin_family = AF_INET;
-	sockname.sin_port = htons(port);
-	sockname.sin_addr.s_addr = htonl(INADDR_ANY);
-	sd=socket(AF_INET,SOCK_STREAM,0);
-	
-	if ( sd == -1)
-		err(1, "socket failed");
-
-	if (bind(sd, (struct sockaddr *) &sockname, sizeof(sockname)) == -1)
-		err(1, "bind failed");
-
-	if (listen(sd,3) == -1)
-		err(1, "listen failed");
-
-	/*
-	 * we're now bound, and listening for connections on "sd" -
-	 * each call to "accept" will return us a descriptor talking to
-	 * a connected client
-	 */
-
-
-	/*
-	 * first, let's make sure we can have children without leaving
-	 * zombies around when they die - we can do this by catching
-	 * SIGCHLD.
-	 */
-	sa.sa_handler = kidhandler;
-        sigemptyset(&sa.sa_mask);
-	/*
-	 * we want to allow system calls like accept to be restarted if they
-	 * get interrupted by a SIGCHLD
-	 */
-        sa.sa_flags = SA_RESTART;
-        if (sigaction(SIGCHLD, &sa, NULL) == -1)
-                err(1, "sigaction failed");
-
-	/*
-	 * finally - the main loop.  accept connections and deal with 'em
-	 */
-	printf("Proxy up and listening for connections on port %u\n", port);
-	
-	for(;;) {
-		
-		int clientsd;
+void *threadFunc(){
+			int clientsd;
 		clientlen = sizeof(&client);
 		clientsd = accept(sd, (struct sockaddr *)&client, &clientlen);
 		
@@ -424,5 +298,137 @@ int main(int argc,  char *argv[])
 			exit(0);
 		}
 		close(clientsd);
+}
+
+int main(int argc,  char *argv[])
+{
+	struct sockaddr_in sockname, client, server_sa;
+	char buffer[80], *ep;
+	size_t maxread;
+	struct sigaction sa;
+	int serverCall[1] = {0};
+	int sd, i, ssd;
+	char bloom[40] = {0};
+	unsigned int fileLen;
+	socklen_t clientlen;
+	u_short port;
+	u_short serverport;
+	pid_t pid;
+	u_long p;
+	u_long sp;
+	struct tls_config *tls_cfg = NULL; // TLS config
+	struct tls_config *tls_scfg = NULL; //TLS server config
+	struct tls *tls_ctx = NULL; // TLS context
+	struct tls *tls_cctx = NULL; // client's TLS context
+	struct tls *tls_sctx = NULL; // server's TLS context
+	
+	
+	/*
+	 * first, figure out what port we will listen on - it should
+	 * be our first parameter.
+	 */
+
+	if (argc != 3)
+		usage();
+		errno = 0;
+        p = strtoul(argv[1], &ep, 10);
+	
+        if (*argv[1] == '\0' || *ep != '\0') {
+		/* parameter wasn't a number, or was empty */
+		fprintf(stderr, "%s - not a number\n", argv[1]);
+		usage();
+	}
+        if ((errno == ERANGE && p == ULONG_MAX) || (p > USHRT_MAX)) {
+		/* It's a number, but it either can't fit in an unsigned
+		 * long, or is too big for an unsigned short
+		 */
+		fprintf(stderr, "%s - value out of range\n", argv[1]);
+		usage();
+	}
+	
+	sp = strtoul(argv[2], &ep, 10);      
+	if (*argv[2] == '\0' || *ep != '\0') {
+		/* parameter wasn't a number, or was empty */
+		fprintf(stderr, "%s - not a number\n", argv[2]);
+		usage();
+	}
+        if ((errno == ERANGE && sp == ULONG_MAX) || (sp > USHRT_MAX)) {
+		/* It's a number, but it either can't fit in an unsigned
+		 * long, or is too big for an unsigned short
+		 */
+		fprintf(stderr, "%s - value out of range\n", argv[2]);
+		usage();
+	}
+	/* now safe to do this */
+	port = p;
+	serverport = sp;
+
+	/* set up TLS */
+	if ((tls_cfg = tls_config_new()) == NULL)
+		errx(1, "unable to allocate TLS config");
+	if (tls_config_set_ca_file(tls_cfg, "/home/csmajs/dgunn001/CS165/TLSCache-master/certificates/root.pem") == -1)
+		errx(1, "unable to set root CA file");
+	if (tls_config_set_cert_file(tls_cfg, "/home/csmajs/dgunn001/CS165/TLSCache-master/certificates/server.crt") == -1) 
+		errx(1, "unable to set TLS certificate file, error: (%s)", tls_config_error(tls_cfg));
+	if (tls_config_set_key_file(tls_cfg, "/home/csmajs/dgunn001/CS165/TLSCache-master/certificates/server.key") == -1)
+		errx(1, "unable to set TLS key file");
+	if ((tls_ctx = tls_server()) == NULL)
+		errx(1, "TLS server creation failed");
+	if (tls_configure(tls_ctx, tls_cfg) == -1)
+		errx(1, "TLS configuration failed (%s)", tls_error(tls_ctx));
+
+	/* the message we send the client */
+	strncpy(buffer,
+	    "It was the best of times, it was the worst of times... \n",
+	    sizeof(buffer));
+
+	
+	memset(&sockname, 0, sizeof(sockname));
+	sockname.sin_family = AF_INET;
+	sockname.sin_port = htons(port);
+	sockname.sin_addr.s_addr = htonl(INADDR_ANY);
+	sd=socket(AF_INET,SOCK_STREAM,0);
+	
+	if ( sd == -1)
+		err(1, "socket failed");
+
+	if (bind(sd, (struct sockaddr *) &sockname, sizeof(sockname)) == -1)
+		err(1, "bind failed");
+
+	if (listen(sd,3) == -1)
+		err(1, "listen failed");
+
+	/*
+	 * we're now bound, and listening for connections on "sd" -
+	 * each call to "accept" will return us a descriptor talking to
+	 * a connected client
+	 */
+
+
+	/*
+	 * first, let's make sure we can have children without leaving
+	 * zombies around when they die - we can do this by catching
+	 * SIGCHLD.
+	 */
+	sa.sa_handler = kidhandler;
+        sigemptyset(&sa.sa_mask);
+	/*
+	 * we want to allow system calls like accept to be restarted if they
+	 * get interrupted by a SIGCHLD
+	 */
+        sa.sa_flags = SA_RESTART;
+        if (sigaction(SIGCHLD, &sa, NULL) == -1)
+                err(1, "sigaction failed");
+
+	/*
+	 * finally - the main loop.  accept connections and deal with 'em
+	 */
+	printf("Proxy up and listening for connections on port %u\n", port);
+	
+	for(;;) {
+		pthread_t tid;
+		pthread_create(&tid, NULL, threadFunc, (void*) &tid);
+		pthread_exit(NULL);
+
 	}
 }
