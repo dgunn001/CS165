3965b7f83a8c2a19f651162fda740556e4b62738
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2020-06-12 00:29:44 -0700

    Update proxy.c

--- TLSCache-master/src/proxy/proxy.c
+++ TLSCache-master/src/proxy/proxy.c
@@ -95,8 +95,8 @@ int bloom_insert (char* bloom, const char* buffer){
 	a = murmurhash2(buffer, len, 17);
 	b = FNVHash(buffer, len);
 	printf("insert: %d %d\n",a,b);
-	bloom[a / 8] ^= 1 << (a & 8 );
-	bloom[b / 8] ^= 1 << (b & 8 );
+	bloom[a / 8] ^= 1 << (a % 8 );
+	bloom[b / 8] ^= 1 << (b % 8 );
 	i = 1 & (bloom[a / 8] >> (a % 8));
 	printf("%d\n", i);
 	return 1;
