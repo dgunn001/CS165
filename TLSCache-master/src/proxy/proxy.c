6b36511414e75e6a87605923f88da614c5059dfa
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2020-06-11 21:46:49 -0700

    Update proxy.c

--- TLSCache-master/src/proxy/proxy.c
+++ TLSCache-master/src/proxy/proxy.c
@@ -16,6 +16,46 @@
 
 int bitVector[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
+static inline uint32_t murmur_32_scramble(uint32_t k) {
+    k *= 0xcc9e2d51;
+    k = (k << 15) | (k >> 17);
+    k *= 0x1b873593;
+    return k;
+}
+uint32_t murmur3_32(const uint8_t* key, size_t len, uint32_t seed)
+{
+	uint32_t h = seed;
+    uint32_t k;
+    /* Read in groups of 4. */
+    for (size_t i = len >> 2; i; i--) {
+        // Here is a source of differing results across endiannesses.
+        // A swap here has no effects on hash properties though.
+        memcpy(&k, key, sizeof(uint32_t));
+        key += sizeof(uint32_t);
+        h ^= murmur_32_scramble(k);
+        h = (h << 13) | (h >> 19);
+        h = h * 5 + 0xe6546b64;
+    }
+    /* Read the rest. */
+    k = 0;
+    for (size_t i = len & 3; i; i--) {
+        k <<= 8;
+        k |= key[i - 1];
+    }
+    // A swap is *not* necessary here because the preceding loop already
+    // places the low bytes in the low places according to whatever endianness
+    // we use. Swaps only apply when the memory is copied in a chunk.
+    h ^= murmur_32_scramble(k);
+    /* Finalize. */
+	h ^= len;
+	h ^= h >> 16;
+	h *= 0x85ebca6b;
+	h ^= h >> 13;
+	h *= 0xc2b2ae35;
+	h ^= h >> 16;
+	return h % 20;
+}
+
 unsigned int FNVHash(char* str, unsigned int length) {
 	const unsigned int fnv_prime = 0x811C9DC5;
 	unsigned int hash = 0;
@@ -219,7 +259,7 @@ int main(int argc,  char *argv[])
 			fileLen = strlen(buffer);
 			unsigned int bloomBit1, bloomBit2;
 			bloomBit1 = FNVHash(buffer, fileLen);
-			bloomBit2 = FNVHash(buffer, fileLen);
+			bloomBit2 = murmur3_32(atoi(buffer), fileLen, 17);
 			printf("Bloom bit locations: %d %d \n", bloomBit1, bloomBit2);
 			
 				
