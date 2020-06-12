f2e6666a23cb5a8ec97a8edf32f8a3692cf1ebf5
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2020-06-11 23:12:41 -0700

    Update proxy.c

--- TLSCache-master/src/proxy/proxy.c
+++ TLSCache-master/src/proxy/proxy.c
@@ -78,7 +78,7 @@ unsigned int FNVHash(const char* str, unsigned int length) {
 void bloom_init (struct bloom * bloom) {
 	unsigned int i;
 	for(i = 0; i < 19; i++){
-		bloom->bitVector[0] = 0;
+		//bloom->bitVector[0] = 0;
 	}
 }
 
